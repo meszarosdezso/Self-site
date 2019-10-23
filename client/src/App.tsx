0ee4d65133860f33f58600e44b0eee442c69821b
meszarosdezso <dezsordog@gmail.com>
2019-10-23 21:20:31 +0200

    Updated homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h2 style={{ color: "#63a0b1" }}>
           Site coming soon ... 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </h2>
       </header>
     </div>
   );
