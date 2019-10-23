4ffc027cbf75320442839d6c7399e39f09fa0f83
meszarosdezso <dezsordog@gmail.com>
2019-10-23 15:13:02 +0200

    Updated text on front page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Site coming soon ... 
         </p>
         <a
           className="App-link"
