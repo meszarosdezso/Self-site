c9ba823f55ba13c850c509feee92470afe3d4cd9
meszarosdezso <dezsordog@gmail.com>
2019-10-24 15:12:46 +0200

    new profile image animation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,19 +3,22 @@ import "./App.scss"
 import CircularAvatar from "./Components/CircularAvatar/CircularAvatar"
 
 const App: React.FC = () => {
-  const [imageUrl, setImageUrl] = useState("")
+  const [profile, setProfile] = useState({ name: "", imageUrl: "" })
 
   useEffect(() => {
     fetch("https://api.github.com/users/meszarosdezso")
       .then(res => res.json())
-      .then(data => setImageUrl(data["avatar_url"]))
+      .then(data =>
+        setProfile({ imageUrl: data["avatar_url"], name: data["name"] })
+      )
   }, [])
 
   return (
     <div className="App">
       <header className="App-header">
-        <CircularAvatar url={imageUrl} alt={"Profile image"} />
-        <h2 style={{ color: "#63a0b1" }}>Site coming soon ...</h2>
+        <CircularAvatar url={profile.imageUrl} alt={"Profile image"} />
+        <br />
+        <h2 style={{ color: "#63a0b1" }}>{profile.name}</h2>
       </header>
     </div>
   )
