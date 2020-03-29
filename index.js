a57f968864555149396d4fcb71c3610823e79d10
meszarosdezso <meszarosdezsodev@gmail.com>
2020-03-29 20:21:13 +0200

    Now try 17

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ mongoose.connect(
 )
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("dist"))
-  console.log("PRODUCTION!!")
+  app.use(express.static("client/build"))
+
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "dist", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   })
 }
 
