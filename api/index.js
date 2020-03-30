2bad0bf0c93a7244eaf0e8516f9bb491fc73c832
meszarosdezso <meszarosdezsodev@gmail.com>
2020-03-30 14:43:41 +0200

    added redirect

--- api/index.js
+++ api/index.js
@@ -23,12 +23,9 @@ mongoose.connect(
 )
 
 if (process.env.NODE_ENV === "production") {
-  res.redirect("https://self-site.now.sh")
-  // app.use(express.static("client/build"))
-
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  // })
+  app.get("*", (req, res) => {
+    res.redirect("https://self-site.now.sh")
+  })
 }
 
 const PORT = process.env.PORT || 8000
