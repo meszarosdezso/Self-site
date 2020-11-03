e7b9acaceb352ab8af52f8bb1db02752946c5391
meszarosdezso <meszarosdezsodev@gmail.com>
2020-11-03 10:23:21 +0100

    🌁 Updated Instagram and dependencies

--- client/components/Instagram/Instagram.tsx
+++ client/components/Instagram/Instagram.tsx
@@ -14,32 +14,41 @@ const Instagram: React.FC<{ posts: InstagramPost[] }> = ({ posts }) => {
       </h1>
 
       <div className="posts">
-        {posts.map((post, i) => (
-          <div key={post.id} className={`post ${i % 2 ? 'right' : 'left'}`}>
-            <div className="image">
-              <img
-                src={post.thumbnail_url || post.media_url}
-                alt={post.caption}
-              />
-            </div>
+        <div className="left">
+          {posts
+            .filter((_, i) => !(i % 2))
+            .map(post => (
+              <Post key={post.id} post={post} />
+            ))}
+        </div>
 
-            <div className="caption sans">
-              <p>
-                <InstagramCaption>{post.caption}</InstagramCaption>
-                <br />
-                <br />
-                {/* 👆 Please don't tell my boss */}
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={post.permalink}
-                >
-                  <ExternalLink />
-                </a>
-              </p>
-            </div>
-          </div>
-        ))}
+        <div className="right">
+          {posts
+            .filter((_, i) => i % 2)
+            .map(post => (
+              <Post key={post.id} post={post} />
+            ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const Post: React.FC<{ post: InstagramPost }> = ({ post }) => {
+  return (
+    <div id={`ig-${post.id}`} key={post.id} className={`post`}>
+      <img src={post.thumbnail_url || post.media_url} alt={post.caption} />
+
+      <div className="caption sans">
+        <p>
+          <InstagramCaption>{post.caption}</InstagramCaption>
+          <br />
+          <br />
+          {/* 👆 Please don't tell my boss */}
+          <a target="_blank" rel="noopener noreferrer" href={post.permalink}>
+            <ExternalLink />
+          </a>
+        </p>
       </div>
     </div>
   )
