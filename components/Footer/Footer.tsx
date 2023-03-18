0d9161ad05892817d67a8259f570b3baf42af772
Dezso Meszaros <meszarosdezsodev@gmail.com>
2023-03-18 09:54:34 +0100

    gears for geeks

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,19 +3,22 @@ import { GitHub } from 'react-feather'
 
 type Props = {
   centered?: boolean
+  noPadding?: boolean
+  hideGithub?: boolean
 }
 
-const Footer: React.FC<Props> = ({ centered }) => {
+const Footer: React.FC<Props> = ({ centered, noPadding, hideGithub }) => {
   return (
     <footer
       style={{ justifyContent: centered ? 'center' : 'space-between' }}
       id={styles.Footer}
+      className={noPadding ? styles['no-padding'] : ''}
     >
       <p id={styles.copyright}>
         Design, code and rights reserved by Dezso Meszaros
       </p>
 
-      {!centered && (
+      {!centered && !hideGithub && (
         <a
           title="Edit this page on Github"
           href="https://github.com/meszarosdezso/Self-site.git"
--- pages/photos/index.tsx
+++ pages/photos/index.tsx
@@ -4,6 +4,13 @@ import Layout from '../../components/Layout/Layout'
 import { Photo } from '../../models/photo'
 import { fetchPhotos } from '../../utils/api'
 import styles from './Photos.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faCamera,
+  faLaptop,
+  faMobilePhone,
+} from '@fortawesome/free-solid-svg-icons'
+import Footer from '../../components/Footer/Footer'
 
 interface Props {
   photos: Photo[]
@@ -12,8 +19,39 @@ interface Props {
 export default function Page({ photos }: Props) {
   return (
     <div className={styles.PhotosPage}>
-      <Layout title="Photos">
+      <Layout title="Photos" hideFooter>
         <Gallery photos={photos} />
+
+        <div className={`${styles.gear} sans`}>
+          {/* <h4>Behind the pictures</h4>   */}
+
+          <ul>
+            <div>
+              <li>
+                <FontAwesomeIcon width={'.9em'} icon={faCamera} />
+              </li>
+              <li>Canon EOS 80D</li>
+              <li>Sigma Art 18-35mm F1.8</li>
+              <li>Sigma Art 50-100mm F1.8</li>
+              <li>Helios 44m F2</li>
+            </div>
+            <div>
+              <li>
+                <FontAwesomeIcon id={styles.phoneIcon} icon={faMobilePhone} />
+              </li>
+              <li>iPhone 14 Pro Max</li>
+              <li>Lightroom Mobile</li>
+            </div>
+            <div>
+              <li>
+                <FontAwesomeIcon width={'.95em'} icon={faLaptop} />
+              </li>
+              <li>Macbook Pro 13" 2021</li>
+              <li>Lightroom CC</li>
+            </div>
+          </ul>
+          <Footer noPadding hideGithub />
+        </div>
       </Layout>
     </div>
   )
