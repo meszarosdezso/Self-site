8989d219d3236a6fdac33b3fb6a4c816ef918e7b
Dezso Meszaros <meszarosdezsodev@gmail.com>
2023-05-03 19:25:06 +0200

    update meta

--- constants/meta.ts
+++ constants/meta.ts
@@ -1,6 +1,6 @@
 export const META_DESCRIPTION =
-  'Budapest based developer, creating web and cross platform mobile apps, user interfaces and experiences.'
+  'Budapest based developer, creating web and cross platform mobile apps, user interfaces and experiences. Currently at UX Studio.'
 export const META_TITLE = 'Dezso Meszaros'
 export const OG_URL = 'https://meszarosdezso.com'
 export const OG_IMAGE = 'https://meszarosdezso.com/littler_me.jpg'
-export const TITLE_BASE = 'Dezso Meszaros - Front-end developer 👨🏽‍💻'
+export const TITLE_BASE = 'Dezso Meszaros - UI/UX Developer'
