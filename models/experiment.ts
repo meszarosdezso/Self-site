6791f45bdfa18c08abcc46394da5b61a5397b2bc
Turbobot <turbobot@vercel.com>
2023-12-16 10:03:57 +0100

    feat(create-turbo): apply official-starter transform

--- models/experiment.ts
+++
@@ -1,8 +0,0 @@
-import { SanityAssetDocument } from '@sanity/client'
-
-export type Experiment = {
-  title: string
-  date: string
-  description: string
-  file: SanityAssetDocument
-}
--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import { AppPropsType } from 'next/dist/shared/lib/utils'
-
-import './_app.scss'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-
-export default function App({ Component, pageProps }: AppPropsType) {
-  return <Component {...pageProps} />
-}
