8a9a8bae4021a5245ca96ec647320e63098aabce
meszarosdezso <meszarosdezsodev@gmail.com>
2023-12-31 08:02:15 +0100

    rm

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -1,16 +1,16 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { GeistMono } from 'geist/font/mono'
+import "./globals.css";
+import type { Metadata } from "next";
+import { GeistMono } from "geist/font/mono";
 
 export const metadata: Metadata = {
-  title: 'Dezso Meszaros',
-  description: '?',
-}
+  title: "Dezso Meszaros",
+  description: "?",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }): JSX.Element {
   return (
     <html lang="en">
@@ -21,14 +21,7 @@ export default function RootLayout({
         className={`${GeistMono.className} overflow-x-hidden bg-dirt dark:bg-midnight text-body dark:text-body-light`}
       >
         {children}
-
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src="/noise.png"
-          loading="eager"
-          className="fixed invisible inset-0 object-cover object-center h-full w-full saturate-0 mix-blend-screen opacity-10 brightness-200 invert pointer-events-none"
-        />
       </body>
     </html>
-  )
+  );
 }
